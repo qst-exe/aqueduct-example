dcc29d7efb119fec3e2e43a9e0d4b03c8274de8d
Task Nagashige <task.19.xx@gmail.com>
2022-03-27 16:12:33 +0900

    first commit

---
+++ lib/aqueduct_example.dart
@@ -0,0 +1,11 @@
+/// aqueduct_example
+///
+/// A Aqueduct web server.
+library aqueduct_example;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
